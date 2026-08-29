5f88bb64c76c934eaec008c0496da59d8b7ee15a
tangidurgaprasad25csm-png <tangidurgaprasad.25.csm@anits.edu.in>
2026-08-29 23:42:58 +0530

    Add files via upload

---
+++ Assignment/Assignment 2/C1ExtC2C3.java
@@ -0,0 +1,24 @@
+package Assignment;
+class C2{
+    void displayC2(){
+        System.out.println("this is C2");
+    }
+}
+class C3{
+     void displyC3(){
+        System.out.println("this is C3");
+     }
+}
+class C1 extends C2 C3{
+      void displayC3(){
+        System.out.println("this is C3");
+      }
+      void displayC1(){
+        System.out.println("this is C1");
+      }
+}
+public class C1ExtC2C3 {
+    public static void main(String[] args) {
+        
+    }
+}
