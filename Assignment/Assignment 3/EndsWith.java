f171259cd7206e855ad4e21b6d54ce1995ecc491
tangidurgaprasad25csm-png <tangidurgaprasad.25.csm@anits.edu.in>
2026-09-12 20:56:39 +0530

    Add files via upload

---
+++ Assignment/Assignment 3/EndsWith.java
@@ -0,0 +1,8 @@
+package Assignment;
+
+public class EndsWith {
+    public static void main(String[] args) {
+       String filename="Class.java";
+       System.out.println("Ends With .java"+filename.endsWith(".java"));
+     }
+}
