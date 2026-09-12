94d2b45b9bb107196cdbc4651c9abd218fef6d27
tangidurgaprasad25csm-png <tangidurgaprasad.25.csm@anits.edu.in>
2026-09-12 21:15:54 +0530

    Add files via upload

---
+++ Assignment/Assignment 3/LowerCase.java
@@ -0,0 +1,9 @@
+package Assignment;
+
+public class LowerCase {
+    public static void main(String[] args) {
+        String collegename="ANITS";
+        System.out.println("original:"+collegename);
+        System.out.println("LowerCase:"+collegename.toLowerCase());
+    }
+}
---
+++ Assignment/Assignment 3/UpperCase.java
@@ -0,0 +1,9 @@
+package Assignment;
+
+public class UpperCase {
+    public static void main(String[] args) {
+        String cityname="vizag";
+        System.out.println("original:"+cityname);
+        System.out.println("UpperCase:"+cityname.toUpperCase());
+    }
+}
