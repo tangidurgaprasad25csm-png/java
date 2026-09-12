d7aa029460780f723930d733c5466ff82db607cd
tangidurgaprasad25csm-png <tangidurgaprasad.25.csm@anits.edu.in>
2026-09-12 22:20:03 +0530

    Add files via upload

---
+++ Assignment/Assignment 3/StringTrim.java
@@ -0,0 +1,9 @@
+package Assignment;
+
+public class StringTrim {
+    public static void main(String[] args) {
+        String adress="srikakulam";
+        System.out.println("before:["+adress+"]");
+        System.out.println("after:["+adress.trim()+"]");
+    }
+}
