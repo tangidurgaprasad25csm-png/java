e30ae885d2c7f7889da3a90cf7b5291a2d21580b
tangidurgaprasad25csm-png <tangidurgaprasad.25.csm@anits.edu.in>
2026-09-12 21:57:09 +0530

    Add files via upload

---
+++ Assignment/Assignment 3/StringReplace.java
@@ -0,0 +1,9 @@
+package Assignment;
+
+public class StringReplace {
+    public static void main(String[] args) {
+        String branch="csd";
+        System.out.println("original:"+branch);
+        System.out.println("updated:"+branch.replace("csd","csm"));
+    }
+}
