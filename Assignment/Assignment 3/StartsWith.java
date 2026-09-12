713d96926dc5ccd4f2fefec85f46c9443e23de95
tangidurgaprasad25csm-png <tangidurgaprasad.25.csm@anits.edu.in>
2026-09-12 21:05:16 +0530

    Add files via upload

---
+++ Assignment/Assignment 3/StartsWith.java
@@ -0,0 +1,8 @@
+package Assignment;
+
+public class StartsWith {
+    public static void main(String[] args) {
+        String websitename="JavaProgramming";
+        System.out.println("starts with Java:"+websitename.startsWith("Java"));
+    }
+}
