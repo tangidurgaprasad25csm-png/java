46d920dc9f79096a86a6c4ba14ad6b3be232c077
tangidurgaprasad25csm-png <tangidurgaprasad.25.csm@anits.edu.in>
2026-09-14 22:11:52 +0530

    Add files via upload

---
+++ Assignment/Assignment 3/SubString.java
@@ -0,0 +1,9 @@
+package Assignment;
+
+public class SubString {
+    public static void main(String[] args) {
+        String ProgramTitle="programming";
+        System.out.println("original:"+ProgramTitle);
+        System.out.println("substring:"+ProgramTitle.substring(0,8));
+    }
+}
